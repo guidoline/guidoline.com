0544c53bb96fae607525dafd4b95db75fe24b53b
Ziopod <ziopod@gmail.com>
2020-01-21 15:23:49 +0100

    (ajout) directive Markdwon pour Vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
+const markdown = require('markdown-it');
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { router, head, isClient}) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  router.options.linkActiveClass = 'is-active'
+  router.options.linkExactActiveClass = 'is-active-exact'
+
+  /**
+   * Custom directive
+   * https://vuejs.org/v2/guide/custom-directive.html
+   */
+  Vue.directive('markdown', {
+    bind: function(el, binding, vnode) {
+
+      if ( binding.value == undefined) {
+        return null;
+      }
+
+      const md = new markdown();
+      const content = md.render(binding.value);
+      el.innerHTML = content;
+    }
+  })
 }
