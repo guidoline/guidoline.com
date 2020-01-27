ac7db3774a7d45bdbe7d3d43f2a617cb1ddd3618
Ziopod <ziopod@gmail.com>
2020-01-27 15:58:14 +0100

    (ajout) page agenda

---
+++ src/components/Event/Abstract.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <h2><g-link :to="post.path">{{ post.title }}</g-link></h2>
+    <div v-markdown="post.excerpt"/>
+  </article>
+</template>
+
+<script>
+import Cover from '@/components/Cover'
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    }
+  },
+ }
+</script>
---
+++ src/pages/Agenda.vue
@@ -0,0 +1,38 @@
+<template>
+  <layout>
+    <ul class="is-quiet" v-for="post in $page.events.edges" :key="post.node.id">
+      <li>
+        <EventAbstract :post="post.node"/>
+      </li>
+    </ul>
+  </layout>
+</template>
+
+<page-query>
+query {
+  events: allPost(limit: 6, filter: {category: { eq: "Évenements"}}) {
+    edges {
+      node {
+        title
+        excerpt
+        path
+        cover {
+          src
+          alt
+          legend
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import EventAbstract from '@/components/Event/Abstract'
+
+export default {
+  components: {
+    EventAbstract
+  }
+}
+</script>
