e0c307765a9ae27896db07ff79ac0a3bb0e509de
Ziopod <ziopod@gmail.com>
2021-10-26 23:06:44 +0200

    Amélioration du Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,34 @@
 <template>
-  <header class="navigation">
-    <h1 class="navigation-brand">
-      <g-link :to="siteURL">{{ siteName }}</g-link>
-    </h1>
-    <Navigation
-      :links="settings.navigation.main"
-      :use-burger="true"
-    />
+  <header class="header">
+    <div class="navigation navigation-sup">
+      <p>
+        <Icon name="mail" /> <a href="mailto:contact@guidoline.com">contact@guidoline.com</a>
+      </p>
+      <p>
+        <Icon name="phone" />
+        Tèl : 06 55 55 55 55
+      </p>
+    </div>
+    <div class="navigation">
+      <h1 class="navigation-brand">
+        <g-link :to="siteURL">{{ siteName }}</g-link>
+      </h1>
+      <Navigation
+        :links="settings.navigation.main"
+        :use-burger="true"
+      />
+    </div>
   </header>
 </template>
 
 <script>
 import Navigation from '@/components/Elements/Navigation'
+import Icon from '@/components/Elements/Icon'
 export default {
+  components: {
+    Navigation,
+    Icon
+  },
   props: {
     /**
      * Réglage globaux
@@ -22,7 +38,6 @@ export default {
       required: true,
     }
   },
-  components: { Navigation },
   computed: {
     /**
      * Le nom du site
@@ -40,9 +55,19 @@ export default {
 </script>
 
 <style>
-.navigation { text-align: center; }
+.navigation {
+  padding: var(--gap) 0;
+  text-align: center;
+  /* Est-ce que le nesting composé fonctionne avec PostCSS ? */
+  &-sup { color: red;}
+}
+
+.navigation-sup {
+    font-size: var(--font-size-small);
+    padding-bottom: 0;
+    & p:last-child { margin-bottom: 0; }
+}
 .navigation-menu-item {
-  --paddings: var(--gap-third) var(--gap-demi);
   display: block;
   padding: var(--paddings);
   text-decoration: none;
@@ -51,15 +76,18 @@ export default {
 /* @media only screen and (min-width: calc(22.5rem * 2)) { */
 @media (--tablet) {
   .navigation {
-    color: red !important;
     display: flex;
     align-items: center;
     justify-content: space-between;
+    & p {
+      margin: 0;
+    }
   }
   .navigation-brand {
     display: flex;
     align-items: center;
     justify-content: flex-start;
+    margin: 0;
   }
   .navigation-menu {
     display: flex;
