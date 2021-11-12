c73c750acef256f0d3ece80ffb89c82566db6506
Ziopod <ziopod@gmail.com>
2021-11-12 22:31:43 +0100

    Présentation des crédits photos

--- src/components/Layout/Cover.vue
+++ src/components/Layout/Cover.vue
@@ -6,6 +6,9 @@
     >
       <div class="cover-supcaption" v-if="cover.supcaption" v-html="markdownify(cover.supcaption)" />
       <div v-html="markdownify(cover.legend)" />
+      <div class="cover-credit" v-if="cover.credit">
+        {{ cover.credit }}
+      </div>
       <z-button
         v-if="hasLink"
         :to="cover.link.href"
@@ -39,9 +42,10 @@ const hasLink = computed(() => {
 .cover { @apply relative min-h-60vh overflow-hidden; }
 .cover.is-full { @apply absolute top-0 w-full h-100vh min-h-84; }
 .cover.is-full :deep(+ *) { @apply text-red-500 mt-100vh; }
-.cover img { @apply absolute w-full h-full object-cover; }
+.cover.is-full img { @apply absolute w-full h-full object-cover; }
 .cover figcaption { @apply absolute bottom-4 left-4 p-4 bg-white text-lg; }
 .cover figcaption :deep(h1) { @apply text-6xl font-medium mb-1; }
-.cover-supcaption { @apply pb-2 text-sm text-gray-400 uppercase }
+.cover-supcaption { @apply pb-2 text-sm text-gray-400 uppercase; }
+.cover-credit { @apply text-sm text-right text-gray-400; }
 .cover.is-full figcaption { @apply mr-4 bottom-20 md:(max-w-screen-sm); }
 </style>
