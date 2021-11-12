05f45060ff43fcb36e70f93968ed9da013b6bae6
Ziopod <ziopod@gmail.com>
2021-11-12 21:47:09 +0100

    Simplification de la grille

--- external/z-components/layout/grid.vue
+++ external/z-components/layout/grid.vue
@@ -1,45 +1,89 @@
 <template>
-  <section class="grid">
+  <div class="z-grid">
     <slot />
-  </section>
+  </div>
 </template>
 <style scoped>
-/* https://gridbyexample.com/examples/ */
-/* Objectifs:
-Permettre de placer simplement des éléments sur une grille.
-Cette grille peut être stylisée (ex. bords exterieurs arrondis)
-Usage : gallery d'images, ligne de boutons
-Propriétés :
- - nombre de colonnes (default : 3)
- - style (arrondis, couleurs, etc.)
- @todo: attedre d'avoir des cas d'usage concret avant d'aller plus loin.
- */
-.grid {
-  /*  Grille automatique */
-  display: grid;
-  /* Répartition auto sur une ligne */
-  grid-auto-flow: column;
-  justify-content: space-between;
-/* grid-template-columns: repeat(auto, 1fr); */
- /* grid-template-rows: repeat(auto, 150px); */
-  /* grid-template-columns: repeat(auto-fill, minmax(30%, 1fr)); */
-  /* grid-auto-columns: auto;
-  grid-auto-columns: minmax(10px, auto);
-  grid-auto-columns: min-content;
-  grid-auto-columns: max-content;
-  grid-auto-columns: 25%;
-  grid-auto-columns: 1fr; */
-  /* grid-auto-flow: column; */
-  /* grid-auto-flow: row; */
-  /* justify-item: stretch; */
-  /* @apply flex space-between items-center; */
-  }
-.grid.multiline {
-  grid-gap: 1rem;
-  /* grid-template-rows: repeat(auto-fill, 120px); */
-  /* grid-template-columns: repeat(auto-fill, minmax(30%, 1fr) ) ; */
-  grid-template-columns: repeat(auto-fill, minmax(30% , 1fr) ) ;
-  grid-auto-flow: dense;
+/*
+Breakpoint natifs:
+- None	width: 100%;
+- sm (640px)	max-width: 640px;
+- md (768px)	max-width: 768px;
+- lg (1024px)	max-width: 1024px;
+- xl (1280px)	max-width: 1280px;
+- 2xl (1536px)	max-width: 1536px;
+*/
+
+  /* grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+  grid-template-rows: repeat(6, 200px); */
+
+/* /Volumes/Big Mama/Sites/ecriturdom/assets/css/layouts/columns.css */
+.cols > div,
+.columns > div { @apply bg-red-200 p-4; }
+/* Gouttières 16px > 24px  */
+.z-grid {
+  /* @apply bg-red-100;  */
+  @apply grid gap-4 ) ;
 }
+.z-grid {
+  grid-template-columns: repeat(auto-fit, minmax(28ch, 1fr));
+  grid-template-rows: repeat(auto-fit, min-content);
+}
+
+.is-container {
+  @apply w-auto
+  mx-4
+  md:(w-screen-md mx-auto gap-6)
+  lg:(w-screen-lg)
+  xl:(w-screen-xl)
+  2xl:(w-screen-2xl);
+}
+
+.is-twelve { @apply grid-cols-12;}
+/* <grid-item span-demi > Example </grid-item> */
+/* .z-grid:deep(.grid-item), */
+.z-grid:deep([class^=col-]),
+.z-grid:deep(.col-span-full) { @apply col-span-full; }
+.z-grid:deep(.col-span-five-sixths) { @apply col-span-10; }
+.z-grid:deep(.col-span-three-quarters) { @apply col-span-9; }
+.z-grid:deep(.col-span-two-thirds) { @apply col-span-8; }
+.z-grid:deep(.col-span-demi) { @apply col-span-6; }
+.z-grid:deep(.col-span-third) { @apply col-span-4; }
+.z-grid:deep(.col-span-quarter) { @apply col-span-3; }
+.z-grid:deep(.col-span-sixth) { @apply col-span-2; }
+
+/* start / end */
+/* @note: ce n'est pas prioritaire, préférez mettre en œuvre l'alignement (plus utile). */
+.z-grid:deep(.col-start-sixth) { @apply col-start-3; }
+.z-grid:deep(.col-start-quarter) { @apply col-start-4; }
+.z-grid:deep(.col-start-third) { @apply col-start-5; }
+.z-grid:deep(.col-start-demi) { @apply col-start-7; }
+.z-grid:deep(.col-start-two-thirds) { @apply col-start-9; }
+.z-grid:deep(.col-start-three-quarters) { @apply col-start-10; }
+.z-grid:deep(.col-start-five-sixths) { @apply col-start-11; }
+
+/* Alignement */
+/* @todo: faire la même chose pour toutes les largeurs */
+/* @todo: tenter d'aligner des élement implicitement, par ex.
+   deux boutons « avant / après » alignés à gauche et à droite.
+   Teset d'autre cas de figure, par ex. deux 1/2 avec texte aligné à gauche et
+   text aligné à droite.
+   Prévir également le comportement responsive.
+ */
+.z-grid:deep(.col-span-five-sixths.col-center) { @apply col-start-2; }
+.z-grid:deep(.col-span-five-sixths.col-right) { @apply col-start-3; }
+.z-grid:deep(.col-span-three-quarters.col-center) { @apply col-start-2 col-span-10; }
+.z-grid:deep(.col-span-three-quarters.col-right) { @apply col-start-4; }
+.z-grid:deep(.col-span-two-thirds.col-center) { @apply col-start-3; }
+.z-grid:deep(.col-span-two-thirds.col-right) { @apply col-start-5; }
+.z-grid:deep(.col-span-demi.col-center) { @apply col-start-4; }
+.z-grid:deep(.col-span-demi.col-right) { @apply col-start-7; }
+.z-grid:deep(.col-span-third.col-center) { @apply col-start-5; }
+.z-grid:deep(.col-span-third.col-right) { @apply col-start-9; }
+.z-grid:deep(.col-span-quarter.col-center) { @apply col-start-5 col-span-4; }
+.z-grid:deep(.col-span-quarter.col-right) { @apply col-start-10; }
+.z-grid:deep(.col-span-sixth.col-center) { @apply col-start-6; }
+.z-grid:deep(.col-span-sixth.col-right) { @apply col-start-11; }
+
 
 </style>
