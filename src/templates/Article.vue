fbfd85afa2d379386d156e267f4ba4241686f0a5
Ziopod <ziopod@gmail.com>
2021-11-13 03:39:38 +0100

    Affchage de métas données

--- src/templates/Article.vue
+++ src/templates/Article.vue
@@ -20,23 +20,26 @@ const props = defineProps({
  * @todo: À transformer en filtre de modèle
  */
 
+const cover = computed(() => {
+  if (!props.frontmatter.cover) return null
+  return {
+    ...props.frontmatter.cover,
+    // supcaption: `Publié le ${props.frontmatter.date}`
+  }
+})
+
 const category = computed(() => {
   if (!props.frontmatter.category) return null
   return stringsToURL('/journal/categorie/', [props.frontmatter.category]).shift()
 })
 
-const tags = computed(() => {
+// Pas nécessaire de compilé ?
+const tags = (() => {
   if (!props.frontmatter.tags) return null
   return stringsToURL('/journal/etiquette/', props.frontmatter.tags)
-})
+})()
 
-const license = computed(() => {
-  if (!props.frontmatter.license) return null
-  return {
-    to: props.frontmatter.license.href,
-    name: props.frontmatter.license.nale
-  }
-})
+const date = props.frontmatter.date;
 
 /**
  * Author
@@ -54,48 +57,53 @@ const author = computed(() => {
   <main
     :class="frontmatter.cover ? `has-cover`: null"
   >
-    <Cover :cover="frontmatter.cover" />
+    <Cover v-if="cover" :cover="cover" />
     <div class="prose-container">
+      <div v-if="date" class="article-date">
+        Publié le {{ date }}
+      </div>
       <slot />
     </div>
-    <footer class="article-info">
-      <section v-if="category">
-        <h1 class="article-info-title">Catégorie</h1>
-        <router-link :to="category.to">
-          {{ category.name }}
-        </router-link>
-      </section>
-      <section v-if="tags">
-        <h1 class="article-info-title">Étiquettes</h1>
-        <div class="tags">
-          <router-link
-            v-for="tag in tags"
-            :key="tag.to"
-            :to="tag.to"
-          >
-            {{ tag.name }}
+    <footer class="prose-container">
+      <z-grid class="article-info ">
+        <section v-if="category">
+          <h1 class="article-info-title">Catégorie</h1>
+          <router-link :to="category.to">
+            {{ category.name }}
           </router-link>
-        </div>
-      </section>
-      <section v-if="author">
-        <h1 class="article-info-title">Auteur</h1>
-        <router-link
-          v-if="author.to"
-          :to="author.to"
-        >
-         {{ author.name }}
-        </router-link>
-        <template v-else>
+        </section>
+        <section v-if="tags">
+          <h1 class="article-info-title">Étiquettes</h1>
+          <span class="tags">
+            <router-link
+              v-for="tag in tags"
+              :key="tag.to"
+              :to="tag.to"
+            >
+              {{ tag.name }}
+            </router-link>
+          </span>
+        </section>
+        <section v-if="author">
+          <h1 class="article-info-title">Auteur</h1>
+          <router-link
+            v-if="author.to"
+            :to="author.to"
+          >
           {{ author.name }}
-        </template>
-      </section>
+          </router-link>
+          <template v-else>
+            {{ author.name }}
+          </template>
+        </section>
+      </z-grid>
     </footer>
     <!-- @ajouter : auteur, date, tags, category, related_posts, aticle suivat / précedent-->
   </main>
 </template>
 <style scoped>
-.article-info-title { @apply uppercase text-xs text-gray-400; }
-.article-info { @apply p-4 bg-white text-sm text-gray-500 font-light; }
+.article-info-title { @apply inline pr-2 uppercase text-xs text-gray-400; }
+.article-info { @apply bg-white text-sm text-gray-500 font-light; }
 .tags a:not(:last-child):after {
   content: ", ";
 }
