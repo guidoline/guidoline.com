24e6dd0b4607215de4de6b663c1ad6bfc403d6af
Ziopod <ziopod@gmail.com>
2022-03-29 18:21:25 +0200

    Amélioration de la couverture

--- src/components/Layout/Cover.vue
+++ src/components/Layout/Cover.vue
@@ -4,19 +4,19 @@
     <figcaption
       v-if="cover.legend"
     >
-      <div class="cover-supcaption" v-if="cover.supcaption" v-html="markdownify(cover.supcaption)" />
-      <div v-html="markdownify(cover.legend)" />
-      <div class="cover-credit" v-if="cover.credit">
-        {{ cover.credit }}
-      </div>
+      <div class="cover-supcaption" v-if="cover.supcaption">{{ cover.supcaption }}</div>
+      <div class="cover-legend" v-html="markdownify(cover.legend)" />
       <z-button
         v-if="hasLink"
         :to="cover.link.href"
         :icon="cover.link.icon"
-        :type="cover.link.color"
+        type="filled"
+        :class="cover.link.color"
+
       >
         {{ cover.link.text }}
       </z-button>
+      <div class="cover-credit" v-if="cover.credit">{{ cover.credit }}</div>
     </figcaption>
   </figure>
 </template>
@@ -39,14 +39,18 @@ const hasLink = computed(() => {
 })
 </script>
 <style scoped>
-.cover { @apply relative min-h-60vh overflow-hidden; }
+.cover { @apply relative min-h-70 max-h-60vh overflow-hidden; }
 .cover.is-full { @apply absolute top-0 w-full h-100vh min-h-94; }
 .cover.is-full :deep(+ *) { @apply mt-100vh; }
 .cover img { @apply w-full min-h-25rem object-cover; }
 .cover.is-full img { @apply absolute h-full min-h-100vh; }
-.cover figcaption { @apply absolute bottom-4 left-4 p-4 bg-day-background text-day-text text-lg; }
+.cover figcaption { @apply absolute bottom-4 left-4 p-4 bg-day-background text-day-text; }
 .cover figcaption :deep(h1) { @apply text-6xl font-medium mb-1; }
-.cover-supcaption { @apply pb-2 text-sm text-day-text uppercase; }
-.cover-credit { @apply text-sm text-gray-400; }
+.cover-supcaption { @apply pb-2 text-sm text-day-text-light uppercase; }
+.cover-legend { @apply text-day-text; }
+.cover-legend :deep(h2) { @apply text-lg font-bold; }
+.cover-legend :deep(p) { @apply mb-4; }
+.cover-legend:last-child :deep(p) { @apply mb-0;}
+.cover-credit { @apply text-sm italic text-day-background-darker mt-2; }
 .cover.is-full figcaption { @apply mr-4 bottom-20 md:(max-w-screen-sm); }
 </style>
