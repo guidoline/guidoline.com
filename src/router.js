0f3a3050016649cf1af839f88f8b78782c541908
Ziopod <ziopod@gmail.com>
2022-04-06 16:28:10 +0200

    Correction

--- src/router.js
+++ src/router.js
@@ -102,7 +102,7 @@ const routes = Object.keys(pages)
         props = (route) => ({ content: route.meta.props.content })
         beforeEnter = (to, from, next) => {
           const article = articlesStore.getArticle(to.path)
-          if (!content) {
+          if (!article) {
             next({ name: '404', params: [to.path] })
           } else {
             Object.assign(to.meta, { props: { content: article } })
