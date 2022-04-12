36ee47c053df7c81baedc7e185b49957d48e53a1
Ziopod <ziopod@gmail.com>
2022-04-12 21:21:38 +0200

    Ajout d'un watchdog

--- src/pages/Blog/Categories.vue
+++ src/pages/Blog/Categories.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Title from '~/components/Layout/Title.vue'
 import Navigation from '~/components/Blog/Navigation.vue'
+import { computed } from 'vue'
 
 import { useArticlesStore } from '~/store/modules/articles.js'
 const store = useArticlesStore()
 store.initialize()
-const categories = store.getCategories()
+const categories = computed(() => store.getCategories())
 </script>
 <template>
   <LayoutDefault>
@@ -13,14 +14,19 @@ const categories = store.getCategories()
     <main>
       <Title>Categories</Title>
       <div class="prose container">
-        <z-tag
-          v-for="(category, index) in categories"
-          :key="index"
-          :to="category.to"
-          class="is-large is-secondary"
-        >
-         {{ category.name }}
-        </z-tag>
+        <template v-if="categories.length">
+          <z-tag
+            v-for="(category, index) in categories"
+            :key="index"
+            :to="category.to"
+            class="is-large is-secondary"
+          >
+          {{ category.name }}
+          </z-tag>
+        </template>
+        <p v-else class="text-center italic">
+          Aucune publication.
+        </p>
       </div>
     </main>
   </LayoutDefault>
--- src/pages/Blog/Etiquettes.vue
+++ src/pages/Blog/Etiquettes.vue
@@ -1,10 +1,11 @@
 <script setup>
 import Title from '~/components/Layout/Title.vue'
 import Navigation from '~/components/Blog/Navigation.vue'
+import { computed } from 'vue'
 import { useArticlesStore } from '~/store/modules/articles.js'
 const store = useArticlesStore()
 store.initialize()
-const tags = store.getTags()
+const tags = computed(() => store.getTags())
 </script>
 <template>
   <LayoutDefault>
@@ -12,14 +13,19 @@ const tags = store.getTags()
     <main>
       <Title>Étiquettes</Title>
       <div class="prose container">
-        <z-tag
-          v-for="(tag, index) in tags"
-          :key="index"
-          :to="tag.to"
-          class="is-large is-secondary"
-        >
-         {{ tag.name }}
-        </z-tag>
+        <template v-if="tags.length">
+          <z-tag
+            v-for="(tag, index) in tags"
+            :key="index"
+            :to="tag.to"
+            class="is-large is-secondary"
+          >
+          {{ tag.name }}
+          </z-tag>
+        </template>
+        <p v-else class="text-center italic">
+          Aucune publication.
+        </p>
       </div>
     </main>
   </LayoutDefault>
