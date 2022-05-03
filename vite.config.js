858d917060ec05c761e2c0c555372766115f6648
Ziopod <ziopod@gmail.com>
2022-05-03 18:32:12 +0200

    Générer le manifeste prend des ressoruces inutilement

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig(({ command, mode }) => ({
   ],
   server: { fs: { strict: true } },
   build: {
-    sourcemap: true,
+    sourcemap: false,
     manifest: true,
     ssrManifest: true
   },
