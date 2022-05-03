7bbf6e02df4ca7334a4017f7d6dc26a5b47561ec
Ziopod <ziopod@gmail.com>
2022-05-03 17:34:40 +0200

    Supression de class inutilisée

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -64,7 +64,6 @@ const changePage = (folio) => {
         @change-folio="changePage($event)"
         :folio="folio"
         :folio-count="pagesCount"
-        link-classes="text"
       />
       <BlogFooter
         :articles-count="articlesCount"
