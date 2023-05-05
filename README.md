fc12d491a0ef2b2319f54076b3ae922ed54f8c1e
Ziopod <ziopod@gmail.com>
2023-05-05 06:39:19 +0200

    Documentation au sujet du bug de build de NextJS v13.4

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 Simple stack KISS
 
-Avec focus sur la simplicieté de maitnenance et la performance.
+Avec focus sur la simplicieté de maintenance et la performance.
+
+## Stack
+
+NextJS 13 avec la fonctionnalité expérimentale `appDir` ([présentation de appDir](https://blog.logrocket.com/next-js-13-app-directory/)).
+
 
 ## Hebergement
 Vercel
--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // @note: les 3 paramètres suivants génèrent une erreur lors
+  // du build (dû aux routes dynamiques).
+  // cf. https://github.com/vercel/next.js/issues/49059
+  // output: 'export',
+  // trailingSlash: true,
+  // distDir: 'dist',
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
   },
   experimental: {
     appDir: true
-  }
+  },
 }
 
 module.exports = nextConfig
