bcc0f995711eae35395ff2640a7b80a5087dfcd8
Ziopod <ziopod@gmail.com>
2023-05-06 02:27:50 +0200

    Illustration de l'usage d'un ficheir de donnée JSON

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,10 +1,37 @@
 import Link from "next/link";
+import settingsGlobal from "../../content/settings/global.json"
+
+// Note: Ce fichier illustre l'usage d'un fichier Markdown pour afficher
+// diverse type de données.
 
 export function Footer() {
+  const contactRouen = settingsGlobal.contacts.main
+  // @todo: affocher l'adresse de Sotteville
   return (
     <footer>
-      <pre>FOOTER</pre>
-      <Link href="/mentions-legales">Mentions légales</Link>
+      <section>
+        <h1>{settingsGlobal.name}</h1>
+        <div dangerouslySetInnerHTML={{__html:settingsGlobal.about}} />
+      </section>
+      <section>
+        <h1>{contactRouen.name}</h1>
+        <p>{contactRouen.description}</p>
+        <address>
+          {contactRouen.address.street}<br/>
+          {contactRouen.address.zipcode} {contactRouen.address.city}
+        </address>
+        <p>
+          <strong>Horaires</strong><br/>
+          {contactRouen.hours}
+        </p>
+        <p>
+          <a href={`mailto:${contactRouen.email}`}>{contactRouen.email}</a><br/>
+          <a href={`tel:${contactRouen.phone}`}>{contactRouen.phone}</a>
+        </p>
+      </section>
+      <section>
+        <Link href="/mentions-legales">Mentions légales</Link>
+      </section>
     </footer>
   )
 }
